cb9655bda10f979e6c105f3daedfe1968d5398af
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2020-12-25 19:00:38 +0100

    Implementation of showMeetingDetails function in the MeetingDetailsFragment

--- app/src/main/java/com/example/mareu/fragment/MeetingDetailsFragment.java
+++ app/src/main/java/com/example/mareu/fragment/MeetingDetailsFragment.java
@@ -7,8 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.mareu.R;
+import com.example.mareu.model.Meeting;
+
+import java.util.List;
 
 
 public class MeetingDetailsFragment extends Fragment {
@@ -28,7 +32,30 @@ public class MeetingDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_meeting_details, container, false);
 
+        Meeting meeting = new Meeting();
+        showMeetingDetails(view, meeting);
 
         return view;
+    }
+
+    private void showMeetingDetails(View view, Meeting meeting){
+        TextView roomText  = view.findViewById(R.id.room_detail_text);
+        TextView timeText = view.findViewById(R.id.time_detail_text);
+        TextView subjectText = view.findViewById(R.id.subject_detail_text);
+        TextView participantsEmailsText = view.findViewById(R.id.participants_emails_text);
+
+        StringBuilder emailsText = new StringBuilder();
+        List<String> emailList = meeting.getParticipantMailList();
+        for (int i = 0; i < emailList.size(); i++) {
+            if (i == 0)
+                emailsText = new StringBuilder(emailList.get(i));
+            else
+                emailsText.append("\n").append(emailList.get(i));
+        }
+
+        roomText.setText(meeting.getPlace());
+        timeText.setText(meeting.getTime());
+        subjectText.setText(meeting.getSubject());
+        participantsEmailsText.setText(meeting.getParticipantMailList().toString());
     }
 }
