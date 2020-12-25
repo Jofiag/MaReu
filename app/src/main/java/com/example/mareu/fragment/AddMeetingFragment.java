7c0de296e8be7bca9a5f80ac0e68318d6f6fdcb6
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2020-12-25 20:20:35 +0100

    Create ,xml file for tablet screen.

--- app/src/main/java/com/example/mareu/fragment/AddMeetingFragment.java
+++ app/src/main/java/com/example/mareu/fragment/AddMeetingFragment.java
@@ -13,7 +13,7 @@ import com.example.mareu.R;
 public class AddMeetingFragment extends Fragment {
 
     public AddMeetingFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -23,9 +23,11 @@ public class AddMeetingFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_meeting, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_meeting, container, false);
+
+
+
+        return view;
     }
 }
