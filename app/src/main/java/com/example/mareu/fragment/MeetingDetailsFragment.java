97a3d6e951ae4f0b763663110c2edf7f5627f1a3
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2020-12-25 17:04:09 +0100

    Create MeetingDetailsFragment

---
+++ app/src/main/java/com/example/mareu/fragment/MeetingDetailsFragment.java
@@ -0,0 +1,34 @@
+package com.example.mareu.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mareu.R;
+
+
+public class MeetingDetailsFragment extends Fragment {
+
+
+    public MeetingDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_meeting_details, container, false);
+
+
+        return view;
+    }
+}
