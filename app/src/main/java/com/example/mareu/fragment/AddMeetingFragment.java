fa70bb03687ff41eb11f99e946c4d1c3c666e710
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2020-12-26 13:41:07 +0100

    Deleting the participant email edit text in the add meeting fragment. We are going to use the popup to do so.

--- app/src/main/java/com/example/mareu/fragment/AddMeetingFragment.java
+++ app/src/main/java/com/example/mareu/fragment/AddMeetingFragment.java
@@ -7,15 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.mareu.R;
 
 public class AddMeetingFragment extends Fragment {
 
     public AddMeetingFragment() {
-
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +24,7 @@ public class AddMeetingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_meeting, container, false);
+
 
 
 
